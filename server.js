2d0320323fda5aff6d8e2ed23c6ed8760f59b24e
Aryaman1706 <agrover1_be19@thapar.edu>
2020-11-25 20:34:27 +0530

    fix

--- server.js
+++ server.js
@@ -76,8 +76,9 @@ app.use("/api/call", call);
 
 // * Deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname + "/client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  app.use(express.static(path.join(__dirname, "build")));
+
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 }
