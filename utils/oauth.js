e8669f2ff17337531c712d324784027b441bae66
Aryaman1706 <agrover1_be19@thapar.edu>
2020-11-25 17:12:09 +0530

    oauth

--- utils/oauth.js
+++ utils/oauth.js
@@ -4,7 +4,7 @@ exports.getAccessToken = async (code) => {
   const { data } = await axios.post("https://oauth2.googleapis.com/token", {
     client_id: process.env.OAUTH_CLIENT_ID,
     client_secret: process.env.OAUTH_CLIENT_SECRET,
-    redirect_uri: "http://localhost:5000/api/user/auth/callback",
+    redirect_uri: "https://yogaq.herokuapp.com/api/user/auth/callback",
     grant_type: "authorization_code",
     code,
   });
