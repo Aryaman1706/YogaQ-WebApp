516c53d1ddd7bfea8d61530961bb3b7acf8eb58a
Aryaman1706 <agrover1_be19@thapar.edu>
2020-11-25 18:51:05 +0530

    fix

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   // * Serve Frontend
-  app.use("*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
