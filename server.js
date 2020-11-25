d9a8ebde010e40cb793ec7c603adbd73e4dc5920
Aryaman1706 <agrover1_be19@thapar.edu>
2020-11-25 16:47:11 +0530

    prod fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
-require("dotenv").config();
 require("./config/passport");
 const session = require("express-session");
 const cors = require("cors");
